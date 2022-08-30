7a7f2109fc78a2afbe396f0aafa85c392881e1f3
Enes Kaya <eng.eneskaya@gmail.com>
2022-08-30 13:30:21 +0300

    Initial Commit

---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by Enes Kaya on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
